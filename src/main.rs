5907cd4ff06367be44f8fd9b3188463a086c526d
Tristan Mortimer <tristan.e.mortimer@gmail.com>
2023-12-05 20:12:13 -0500

    Change most of the panic error handling to properly emit results with debug prints and improve overall formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use tokio::sync::Mutex;
-use serde::{Serialize, Deserialize};
 use rocket::{
-    http::{Cookie, CookieJar, Status, 
-        uri::{Uri, Absolute}},
+    http::{Cookie, CookieJar, Status, uri::{Uri, Absolute}},
     form::Form,
     request::{FromRequest, Outcome},
-    response::{Redirect, status::{NotFound, Accepted}},
+    response::{Redirect, Debug},
     fs::{FileServer, relative},
     State, request, form, Config};
+use tokio::sync::Mutex;
+use serde::{Serialize, Deserialize};
+use anyhow::{Error, Result, anyhow, Context};
 use reqwest::Client;
 use rocket_dyn_templates::{Template, context};
 use figment::{Figment, providers::Env};
@@ -27,8 +27,8 @@ const GH_COMMITTER_NAME: &str = "Webring Bot";
 const GH_COMMITTER_EMAIL: &str = "recurseringbot@server.fake";
 
 const RECURSE_BASE_URL: &str = "https://www.recurse.com/api/v1/";
-const RECURSE_OAUTH_URL: &str = "https://www.recurse.com/oauth/authorize";
-const RECURSE_TOKEN_URL: &str = "https://www.recurse.com/oauth/token";
+const _RECURSE_OAUTH_URL: &str = "https://www.recurse.com/oauth/authorize";
+const _RECURSE_TOKEN_URL: &str = "https://www.recurse.com/oauth/token";
 
 #[derive(Deserialize)]
 struct ClientTokens {
@@ -70,6 +70,7 @@ impl<'r> FromRequest<'r> for User {
             .guard::<&CookieJar<'_>>()
             .await
             .expect("Cookies should be accessible from the request");
+
         match (cookies.get_private("name"), cookies.get_private("id"), cookies.get_private("api_token")) {
             (Some(name), Some(id), Some(token)) => 
                 Outcome::Success(User {
@@ -81,35 +82,50 @@ impl<'r> FromRequest<'r> for User {
     }
 }
 
-fn valid_domain<'v>(input_url: &String) -> form::Result<'v, ()> {
-    let maybe_uri = Uri::parse::<Absolute>(input_url.as_str());
-    let parsed_uri = maybe_uri.unwrap();
+fn valid_domain<'v>(input_url: &str) -> form::Result<'v, ()> {
+    let unparseable_domain_message = "Please use a parseable domain name";
+    let parsed_uri = Uri::parse::<Absolute>(input_url)
+        .map_err(|_| form::Error::validation(unparseable_domain_message))?;
+
+    let invalid_domain_message = "Please use a valid domain name";
     let host_domain = parsed_uri
-        .absolute().unwrap()
-        .authority().unwrap()
+        .absolute()
+        .ok_or_else(|| { form::Error::validation(invalid_domain_message) })?
+        .authority()
+        .ok_or_else(|| { form::Error::validation(invalid_domain_message) })?
         .host();
     let domain = parse_domain_name(host_domain)
-        .or_else(|e| { println!("{}", e); Err(form::Error::validation("Please use a parseable domain name")) })? ;
+        .map_err(|_| form::Error::validation(unparseable_domain_message))?;
     
     if !domain.has_known_suffix() {
-        Err(form::Error::validation("Please use a valid domain name"))?;
+        Err(form::Error::validation(invalid_domain_message))?;
     }
 
     Ok(())
 }
 
-async fn get_site_content(octocrab: &Mutex<Octocrab>) -> Content {
+async fn get_site_content(octocrab: &Mutex<Octocrab>) -> Result<Content> {
     let all_contents = octocrab.lock().await.repos(GH_USER, GH_REPO)
-    .get_content()
-    .path(GH_SITES_PATH)
-    .r#ref("main")
-    .send()
-    .await.unwrap();
+        .get_content()
+        .path(GH_SITES_PATH)
+        .r#ref("main")
+        .send().await?;
 
-    all_contents.items.first().unwrap().clone()
+    all_contents.items.into_iter()
+        .next()
+        .ok_or(anyhow!("Missing GitHub content for {}", GH_SITES_PATH))
 }
 
-async fn get_named_sites(unnamed_sites: Vec<SiteData>, bearer_token: &String) -> Vec<SiteData> {
+async fn get_deserialized_sites(octocrab: &Mutex<Octocrab>) -> Result<Vec<SiteData>> {
+    let decoded_sites = get_site_content(octocrab).await?
+        .decoded_content()
+        .ok_or(anyhow!("Could not decode GitHub content"))?;
+
+    serde_json::from_str(&decoded_sites)
+        .context("Unable to deserialize sites from GitHub file")
+}
+
+async fn get_named_sites(unnamed_sites: Vec<SiteData>, bearer_token: &String) -> Result<Vec<SiteData>> {
     let mut sites_with_names = Vec::new();
 
     for site in unnamed_sites {
@@ -117,10 +133,10 @@ async fn get_named_sites(unnamed_sites: Vec<SiteData>, bearer_token: &String) ->
             let response = Client::new()
                 .get(format!("{}profiles/{}", RECURSE_BASE_URL, site.recurse_id))
                 .bearer_auth(bearer_token)
-                .send().await.unwrap();
-            let res = response.text().await.unwrap();
+                .send().await?;
+            let res = response.text().await?;
             let Ok(user) = serde_json::from_str::<User>(res.as_str()) else {
-                return Vec::new();
+                return Ok(Vec::new());
             };
 
             sites_with_names.push(SiteData { 
@@ -134,32 +150,33 @@ async fn get_named_sites(unnamed_sites: Vec<SiteData>, bearer_token: &String) ->
         }
     }
 
-    return sites_with_names;
+    Ok(sites_with_names)
 }
 
 #[get("/?<id>&<uuid_str>")]
-async fn authed(user: User, gh_client: &State<Mutex<Octocrab>>, id: Option<u32>, uuid_str: Option<String>) -> Template {
-    let encoded_sites = get_site_content(gh_client.inner()).await;
-    let site_data: Vec<SiteData> = serde_json::from_str(encoded_sites.decoded_content().as_ref().unwrap()).unwrap();
-    let sites_with_names = get_named_sites(site_data, user.token.as_ref().unwrap()).await;
+async fn authed(user: User, gh_client: &State<Mutex<Octocrab>>, id: Option<u32>, uuid_str: Option<String>) -> Result<Template, Debug<Error>> {
+    let recurse_sites: Vec<SiteData> = get_deserialized_sites(gh_client).await?;
+    let sites_with_names = get_named_sites(recurse_sites, user.token.as_ref().unwrap()).await?;
 
-    Template::render("index", context! { sites: sites_with_names, user, id, uuid_str })
+    Ok(Template::render("index", context! { sites: sites_with_names, user, id, uuid_str }))
 }
 
 #[get("/", rank = 2)]
-async fn home(gh_client: &State<Mutex<Octocrab>>, client_tokens: &State<Mutex<ClientTokens>>) -> Template {
-    let encoded_sites = get_site_content(gh_client.inner()).await;
-    let site_data: Vec<SiteData> = serde_json::from_str(encoded_sites.decoded_content().as_ref().unwrap()).unwrap();
+async fn home(gh_client: &State<Mutex<Octocrab>>, client_tokens: &State<Mutex<ClientTokens>>) -> Result<Template, Debug<Error>> {
+    let recurse_sites: Vec<SiteData> = get_deserialized_sites(gh_client).await?;
+    let sites_with_names = get_named_sites(recurse_sites, &client_tokens.lock().await.recurse_secret).await?;
 
-    let sites_with_names = get_named_sites(site_data, &client_tokens.lock().await.recurse_secret).await;
-    Template::render("index", context! { sites: sites_with_names })
+    Ok(Template::render("index", context! { sites: sites_with_names }))
 }
 
 #[post("/auth/add", data = "<form>")]
-async fn add(user: User, form: Form<WebsiteSignup>, gh_client: &State<Mutex<Octocrab>>) -> Redirect {    
-    let encoded_sites = get_site_content(gh_client.inner()).await;
-    let mut site_data: Vec<SiteData> = serde_json::from_str(encoded_sites.decoded_content().as_ref().unwrap()).unwrap();
-    let max_website_id = site_data.iter().max_by_key(|site| site.website_id).unwrap().website_id;
+async fn add(user: User, form: Form<WebsiteSignup>, gh_client: &State<Mutex<Octocrab>>) -> Result<Redirect, Debug<Error>> {   
+    let sites_sha = get_site_content(gh_client).await?.sha;
+    let mut recurse_sites: Vec<SiteData> = get_deserialized_sites(gh_client).await?;
+    let max_website_id = recurse_sites.iter()
+        .max_by_key(|site| site.website_id)
+        .ok_or(anyhow!("No maximum site found"))?
+        .website_id;
     let uuid_str = Uuid::new_v4().to_string();
     let new_site = SiteData {
         website_id: max_website_id + 1,
@@ -170,9 +187,9 @@ async fn add(user: User, form: Form<WebsiteSignup>, gh_client: &State<Mutex<Octo
         url: form.url.clone(), 
     };
 
-    site_data.push(new_site);
-
-    let output = serde_json::to_string_pretty::<Vec<SiteData>>(&site_data).unwrap();
+    recurse_sites.push(new_site);
+    let output = serde_json::to_string_pretty::<Vec<SiteData>>(&recurse_sites)
+        .context("Could not prettify serialized sites")?;
 
     let commit_author = CommitAuthor {
         name: GH_COMMITTER_NAME.to_string(),
@@ -184,14 +201,14 @@ async fn add(user: User, form: Form<WebsiteSignup>, gh_client: &State<Mutex<Octo
             GH_SITES_PATH,
             format!("Automated Action: Added a new site for '{}'", form.url),
             output,
-            &encoded_sites.sha)
+            &sites_sha)
         .branch("main")
         .commiter(commit_author.clone())
         .author(commit_author)
-        .send()
-        .await.unwrap();
+        .send().await
+        .context("Could not update GitHub site files")?;
 
-    Redirect::to(format!("/?id={}&uuid_str={}", max_website_id + 1, uuid_str))
+    Ok(Redirect::to(format!("/?id={}&uuid_str={}", max_website_id + 1, uuid_str)))
 }
 
 #[get("/auth/logout")]
@@ -203,52 +220,68 @@ fn logout(_user: User, cookies: &CookieJar<'_>) -> Redirect {
 }
 
 #[get("/prev?<id>")]
-async fn prev(id: u32, gh_client: &State<Mutex<Octocrab>>) -> Result<Redirect, NotFound<String>> {
-    let encoded_sites = get_site_content(gh_client.inner()).await;
-    let mut site_data: Vec<SiteData> = serde_json::from_str(encoded_sites.decoded_content().as_ref().unwrap()).unwrap();
-    site_data.sort_by_key(|site| site.website_id);
-    let site_position = site_data.iter().position(|site| site.website_id == id).ok_or(NotFound("Website ID not found".to_owned()))?;
+async fn prev(id: u32, gh_client: &State<Mutex<Octocrab>>) -> Result<Redirect, (Status, Debug<Error>)> {
+    let mut recurse_sites: Vec<SiteData> = get_deserialized_sites(gh_client).await
+        .map_err(|e| (Status::InternalServerError, Debug(e)))?;
+    
+    recurse_sites.sort_by_key(|site| site.website_id);
+    
+    let site_position = recurse_sites.iter().position(|site| site.website_id == id)
+        .ok_or((Status::NotFound, Debug(anyhow!("Website ID not found"))))?;
 
-    let redirect_site = match site_position < 1 {
-        true => site_data.last().unwrap(),
-        false => site_data.get(site_position - 1).unwrap(),
-    };
+    let redirect_site 
+        = if site_position < 1 {
+            recurse_sites.last().unwrap()
+        } else { 
+            recurse_sites.get(site_position - 1).unwrap() 
+        };
 
     Ok(Redirect::to(redirect_site.url.clone()))
 }
 
 #[get("/next?<id>")]
-async fn next(id: u32, gh_client: &State<Mutex<Octocrab>>) -> Result<Redirect, NotFound<String>> {
-    let encoded_sites = get_site_content(gh_client.inner()).await;
-    let mut site_data: Vec<SiteData> = serde_json::from_str(encoded_sites.decoded_content().as_ref().unwrap()).unwrap();
-    site_data.sort_by_key(|site| site.website_id);
-    let site_position = site_data.iter().position(|site| site.website_id == id).ok_or(NotFound("Website ID not found".to_owned()))?;
+async fn next(id: u32, gh_client: &State<Mutex<Octocrab>>) -> Result<Redirect, (Status, Debug<Error>)> {
+    let mut recurse_sites: Vec<SiteData> = get_deserialized_sites(gh_client).await
+        .map_err(|e| (Status::InternalServerError, Debug(e)))?;
+    
+    recurse_sites.sort_by_key(|site| site.website_id);
 
-    let redirect_site = match site_position + 1 == site_data.len()  {
-        true => site_data.first().unwrap(),
-        false => site_data.get(site_position + 1).unwrap(),
-    };
+    let site_position = recurse_sites.iter().position(|site| site.website_id == id)
+        .ok_or((Status::NotFound, Debug(anyhow!("Website ID not found"))))?;
+
+    let redirect_site 
+        = if site_position + 1 == recurse_sites.len()  {
+            recurse_sites.first().unwrap()
+        } else {
+            recurse_sites.get(site_position + 1).unwrap()
+        };
 
     Ok(Redirect::to(redirect_site.url.clone()))
 }
 
 #[get("/rand")]
-async fn random(gh_client: &State<Mutex<Octocrab>>) -> Redirect {
-    let encoded_sites = get_site_content(gh_client.inner()).await;
-    let site_data: Vec<SiteData> = serde_json::from_str(encoded_sites.decoded_content().as_ref().unwrap()).unwrap();
+async fn random(gh_client: &State<Mutex<Octocrab>>) -> Result<Redirect, Debug<Error>> {
+    let site_data: Vec<SiteData> = get_deserialized_sites(gh_client).await?;
     let mut rng = thread_rng();
-    let random_site = &site_data[rng.gen_range(1..site_data.len())];
-    Redirect::to(random_site.url.clone())
+    let non_main_site_index = rng.gen_range(1..site_data.len());
+    let random_site = site_data.into_iter().nth(non_main_site_index).unwrap();
+    Ok(Redirect::to(random_site.url))
 }
 
 #[get("/health")]
-fn health() -> Accepted<String> { Accepted("pong!".to_owned()) }
+fn health() -> String { "pong!".to_owned() }
 
 #[launch]
 #[tokio::main]
 async fn rocket() -> _ {
-    let config: ClientTokens = Figment::from(Env::prefixed("CONFIG_")).extract().expect("Config should be extractable");
-    let octocrab = Octocrab::builder().personal_token(config.github_secret.clone()).build().unwrap();
+    let config: ClientTokens = Figment::from(Env::prefixed("CONFIG_"))
+        .extract()
+        .expect("Config should be extractable");
+
+    let octocrab = Octocrab::builder()
+        .personal_token(config.github_secret.clone())
+        .build()
+        .expect("GitHub client should build using personal token");
 
     rocket::build()
         .manage(Mutex::new(octocrab))
--- src/oauth.rs
+++ src/oauth.rs
@@ -1,10 +1,11 @@
 use rocket::{
     http::{Cookie, CookieJar, SameSite},
     fairing::{ Fairing, AdHoc},
-    response::Redirect,
+    response::{Redirect, Debug}
 };
 use rocket_oauth2::{OAuth2, TokenResponse};
 use reqwest::Client;
+use anyhow::{anyhow, Error, Context};
 
 use crate::User;
 
@@ -18,44 +19,47 @@ pub fn recurse_oauth_fairing() -> impl Fairing {
 }
 
 #[get("/auth/callback")]
-async fn callback(
-    token: TokenResponse<User>,
-    cookies: &CookieJar<'_>,
-) -> Result<Redirect, String> {
+async fn callback(token: TokenResponse<User>, cookies: &CookieJar<'_>) -> Result<Redirect, Debug<Error>> {
     let access_token = token.access_token().to_owned();
     let response = Client::new()
         .get(format!("{}profiles/me", crate::RECURSE_BASE_URL))
         .bearer_auth(access_token.clone())
-        .send().await.unwrap();
+        .send().await
+        .context("Unable to build reqwest client")?;
 
     if !response.status().is_success() {
-        return Err(format!("Got non-success status {}", response.status()));
+        return Err(Debug(anyhow!("OAuth callback returned non-success status: {}", response.status())));
     }
 
-    let user: User = serde_json::from_str(response.text().await.unwrap().as_str()).unwrap();
+    let decoded_content = response.text().await
+        .context("Unable to decode content from OAuth callback")
+        .map_err(Debug::from)?;
+
+    let recurse_user: User = serde_json::from_str(&decoded_content)
+        .context("Unable to deserialize user from OAuth callback")
+        .map_err(Debug::from)?;
 
     cookies.add_private(
-        Cookie::build(("name", user.name))
+        Cookie::build(("name", recurse_user.name))
             .same_site(SameSite::Lax)
-            .build(),
-    );
+            .build());
 
     cookies.add_private(
-        Cookie::build(("id", user.id.to_string()))
+        Cookie::build(("id", recurse_user.id.to_string()))
             .same_site(SameSite::Lax)
-            .build(),
-    );
+            .build());
 
     cookies.add_private(
         Cookie::build(("api_token", access_token))
             .same_site(SameSite::Lax)
-            .build(),
-    );
+            .build());
 
     Ok(Redirect::to("/"))
 }
 
 #[get("/auth/login")]
-fn login(oauth2: OAuth2<User>, cookies: &CookieJar<'_>) -> Redirect {
-    oauth2.get_redirect(cookies, &[]).unwrap()
+fn login(oauth2: OAuth2<User>, cookies: &CookieJar<'_>) -> Result<Redirect, Debug<Error>> {
+    oauth2.get_redirect(cookies, &[])
+        .context("OAuth2 unable to create a redirect given expected cookies and config")
+        .map_err(Debug::from)
 }
